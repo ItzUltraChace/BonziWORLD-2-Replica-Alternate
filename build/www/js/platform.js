18af3893a064939057a3349e20db2327e95e4222
ItzCrazyChace <149985208+ItzUltraChace@users.noreply.github.com>
2024-01-06 18:05:58 -0600

    Update platform.js

--- build/www/js/platform.js
+++ build/www/js/platform.js
@@ -9,53 +9,53 @@ var isiOS = navigator.userAgent.match(/(iPad)|(iPhone)|(iPod)/i) != null;
 var urlGPlay = "https://play.google.com/store/apps/details?id=com.jojudge.bonziworld";
 
 $(function() {
-  var support = {
-    AudioContext: {
-      supported: typeof (
-            window.AudioContext ||
-            window.webkitAudioContext
-          ) != "undefined",
-      message: "Your browser does not support the Web Audio API."
-    }
-  };
+	var support = {
+		AudioContext: {
+			supported: typeof (
+						window.AudioContext ||
+						window.webkitAudioContext
+					) != "undefined",
+			message: "Your browser does not support the Web Audio API."
+		}
+	};
+		
+	var supported = true;
+	var supportKeys = Object.keys(support);
+	for (var i = 0; i < supportKeys.length; i++) {
+		var key = supportKeys[i];
+		var obj = support[key];
+		supported = supported && obj.supported;
+		if (!obj.supported) 
+			$("#unsupp_reasons").append(
+				"<li>" + obj.message + "</li>"
+			);
+	}
+	
+	if (!supported) {
+		$("#page_unsupp").show();
+	}
 
-  var supported = true;
-  var supportKeys = Object.keys(support);
-  for (var i = 0; i < supportKeys.length; i++) {
-    var key = supportKeys[i];
-    var obj = support[key];
-    supported = supported && obj.supported;
-    if (!obj.supported) 
-      $("#unsupp_reasons").append(
-        "<li>" + obj.message + "</li>"
-      );
-  }
+	// if (isChromeBrowser && isDesktop) {
+	// 	$(".app_showcase").append(
+	// 		'<a class="app_chrome" href="' + urlChrome + '">' +
+	// 			'<img src="./img/app/chrome.png" alt="Chrome App" />' +
+	// 		'</a>'
+	// 	);
+	// }
 
-  if (!supported) {
-    $("#page_unsupp").show();
-  }
+	if (!isiOS) {
+		$(".app_showcase").append(
+			'<a class="app_android" href="' + urlGPlay + '">' +
+				'<img src="./img/app/google-play-badge.png" alt="Get it on Google Play." />' +
+			'</a>'
+		);
+	}
 
-  // if (isChromeBrowser && isDesktop) {
-  // 	$(".app_showcase").append(
-  // 		'<a class="app_chrome" href="' + urlChrome + '">' +
-  // 			'<img src="./img/app/chrome.png" alt="Chrome App" />' +
-  // 		'</a>'
-  // 	);
-  // }
-
-  if (!isiOS) {
-    $(".app_showcase").append(
-      '<a class="app_android" href="' + urlGPlay + '">' +
-        '<img src="./img/app/google-play-badge.png" alt="Get it on Google Play." />' +
-      '</a>'
-    );
-  }
-
-  if (!isDesktop) {
-    $(".app_showcase").append(
-      '<a class="app_chrome">' +
-        '<img src="./img/app/desktop.png" alt="Open on PC for the best experience." />' +
-      '</a>'
-    );
-  }
+	if (!isDesktop) {
+		$(".app_showcase").append(
+			'<a class="app_chrome">' +
+				'<img src="./img/app/desktop.png" alt="Open on PC for the best experience." />' +
+			'</a>'
+		);
+	}
 });
